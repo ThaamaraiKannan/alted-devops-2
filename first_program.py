1dbd58d3c0ab1d64312dd02ff3eb4a1874e15408
kannan <abc@gmail.com>
2024-06-27 19:39:38 +0530

    basics

---
+++ first_program.py
@@ -0,0 +1,25 @@
+# variable_1 = "Hi Guys, Good Evening!"
+
+# variable_1 = 10 
+
+# variable_2 = 5-2j
+
+# variable_3 = 55+10j
+
+# print(variable_1 + variable_2 + variable_3)
+
+# string_1 = ["Don't Know", "i know", "you know", "we know"]
+
+string_1 = "i'm going to google it, google"
+string_3 = string_1.split(",")
+string_2 = string_3[0].replace('google', 'search')
+
+print(string_1)
+print(string_2.lower())
+print(string_2.capitalize())
+print(string_2.upper())
+print(string_3)
+
+
+# print(string_2 + ","+ string_3[1])
+# print(string_1)
